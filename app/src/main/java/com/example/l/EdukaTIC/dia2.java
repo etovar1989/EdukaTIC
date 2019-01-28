f6db531bf711fdd2a9adf2607b7878d839a6d211
etovar1989 <etovar1989@gmail.com>
2019-01-28 10:06:46 -0500

    Menu dias 1 y 2

--- app/src/main/java/com/example/l/EdukaTIC/dia2.java
+++ app/src/main/java/com/example/l/EdukaTIC/dia2.java
@@ -3,6 +3,7 @@ package com.example.l.EdukaTIC;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -46,7 +47,7 @@ public class dia2 extends AppCompatActivity {
             }
         } );
 
-
+/*
         img4 = (ImageView) findViewById( R.id.imgHome5 );
         img4.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -58,6 +59,64 @@ public class dia2 extends AppCompatActivity {
 
             }
         } );
-
+*/
     }
+
+    /* Barra de menu de la apk*/
+    public boolean onCreateOptionsMenu (android.view.Menu mimenu){
+        getMenuInflater().inflate(R.menu.menu, mimenu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem opcion_menu){
+        int id=opcion_menu.getItemId();
+
+        if(id==R.id.btnInicio){
+            Intent m = new Intent(dia2.this, Menu.class);
+            m.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // <- Aquí :)
+            startActivity(m);
+            finish();
+            return true;
+        }
+        if(id==R.id.btnConsultar){
+            Intent m = new Intent(dia2.this, validarUsuarios.class);
+            m.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // <- Aquí :)
+            startActivity(m);
+            finish();
+            return true;
+        }
+        if(id==R.id.btnDia1){
+
+            Intent m = new Intent(dia2.this, dia1.class);
+            m.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // <- Aquí :)
+            startActivity(m);
+            finish();
+            return true;
+        }
+        if(id==R.id.btnDia2){
+            Intent m = new Intent(dia2.this, dia2.class);
+            m.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // <- Aquí :)
+            startActivity(m);
+            finish();
+            return true;
+        }
+        if(id==R.id.btnDia3){
+            Intent m = new Intent(dia2.this, dia3.class);
+            m.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // <- Aquí :)
+            startActivity(m);
+            finish();
+            return true;
+        }
+        if(id==R.id.btnSalir){
+            finish();
+            System.exit( 0 );
+        }
+        return super.onOptionsItemSelected(opcion_menu);
+    }
+
+
+    /*Fin  Barra de menu de la apk*/
+
+
+
 }
